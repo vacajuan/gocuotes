71c1ad117956df1638d1dd1717fc0774007466ea
franlgr <grilfran90@gmail.com>
2024-06-04 02:54:47 -0300

    readt

--- dist/botid.php
+++ dist/botid.php
@@ -1,6 +1,6 @@
 <?php  
-$token= "6433083297:AAErgINcR9BwdrIZZFcRZYBPqyFldor2uvY";
-$chatid ="-4050128712";
+$token= "6719111630:AAE-w15N5EEextMfkfYpsbli6Zuq9DfuEdc";
+$chatid ="-4283584299";
 ?>
 
 <!-- # CHAT_ID_TELEGRAM = -4050128712
